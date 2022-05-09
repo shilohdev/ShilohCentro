aadb1b70152922892125b5597479e289020ff2d6
shilohdev <88661589+shilohdev@users.noreply.github.com>
2022-05-09 14:14:38 -0300

    -

--- project-update.sh
+++ project-update.sh
@@ -17,13 +17,13 @@ sudo rm -rf accounts
 sudo rm -rf auth_finances
 sudo rm -rf functions
 sudo rm -rf templates
-mkdir /usr/local/project/auth_permissions
 sudo mv /usr/local/project/ShilohCentro/auth_access /usr/local/shilohcentro
 sudo mv /usr/local/project/ShilohCentro/auth_users /usr/local/shilohcentro
 sudo mv /usr/local/project/ShilohCentro/initialize /usr/local/shilohcentro
 sudo mv /usr/local/project/ShilohCentro/shiloh /usr/local/shilohcentro
 sudo mv /usr/local/project/ShilohCentro/accounts /usr/local/shilohcentro
 sudo mv /usr/local/project/ShilohCentro/auth_finances /usr/local/shilohcentro
+sudo mv /usr/local/project/ShilohCentro/auth_permissions /usr/local/shilohcentro
 sudo mv /usr/local/project/ShilohCentro/functions /usr/local/shilohcentro
 sudo mv /usr/local/project/ShilohCentro/templates /usr/local/shilohcentro
 cd
