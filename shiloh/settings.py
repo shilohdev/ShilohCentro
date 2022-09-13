c4ec2f216cfd79354bd5501bf5d19b528d0b8af1
shilohdev <88661589+shilohdev@users.noreply.github.com>
2022-09-13 12:52:44 -0300

    -

--- shiloh/settings.py
+++ shiloh/settings.py
@@ -103,18 +103,6 @@ AMQP_PASSWORD = os.environ.get("AMQP_PASSWORD")
 AMQP_PORT = os.environ.get("AMQP_PORT")
 AMQP_HOST = os.environ.get("AMQP_HOST")
 
-#CELERY >> config para django
-broker_url = f'amqp://{AMQP_USER}:{AMQP_PASSWORD}@{AMQP_HOST}:{AMQP_PORT}//'
-CELERY_BROKER_URL = f'amqp://{AMQP_USER}:{AMQP_PASSWORD}@{AMQP_HOST}:{AMQP_PORT}//'
-CELERY_ENABLE_UTC = True
-CELERY_BROKER_HEARTBEAT = 0
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'America/Sao_Paulo'
-BROKER_CONNECTION_TIMEOUT = 7200
-CELERY_BROKER_CONNECTION_TIMEOUT = 7200
-CELERY_RESULT_EXPIRES = 7200
 
 
 
@@ -148,7 +136,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'django_celery_beat',
     'initialize',
     'auth_access',
     'auth_users',
